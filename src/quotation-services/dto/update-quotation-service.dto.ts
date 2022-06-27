1f1f7215a5b02d58b20f90dd53a60775c8453caa
akhil272 <akhilms272@gmail.com>
2022-06-28 00:53:03 +0530

    updated quotation service cost patch api

--- src/quotation-services/dto/update-quotation-service.dto.ts
+++ src/quotation-services/dto/update-quotation-service.dto.ts
@@ -1,4 +1,14 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 import { CreateQuotationServiceDto } from './create-quotation-service.dto';
 
-export class UpdateQuotationServiceDto extends PartialType(CreateQuotationServiceDto) {}
+export class UpdateQuotationServiceDto extends PartialType(
+  CreateQuotationServiceDto,
+) {
+  @IsNumber()
+  price: number;
+
+  @IsString()
+  @IsOptional()
+  serviceNote?: string;
+}
